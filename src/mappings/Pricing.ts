0f5d4aa4f68ae377875b87e142e9c0649e4bf84e
matifalcone <matias.falcone@gmail.com>
2021-12-20 13:11:54 +1100

    pricing updates

--- src/mappings/Pricing.ts
+++ src/mappings/Pricing.ts
@@ -3,10 +3,10 @@ import { Pair, Token, Bundle } from '../../generated/schema'
 import { BigDecimal, Address, BigInt } from '@graphprotocol/graph-ts/index'
 import { ZERO_BD, factoryContract, ADDRESS_ZERO, ONE_BD, UNTRACKED_PAIRS } from './Helpers'
 
-const WETH_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
-const USDC_WETH_PAIR = '0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc' // created 10008355
-const DAI_WETH_PAIR = '0xa478c2975ab1ea89e8196811f51a7b7ade33eb11' // created block 10042267
-const USDT_WETH_PAIR = '0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852' // created block 10093341
+const WIOTX_ADDRESS = '0xa00744882684c3e4747faefd68d283ea44099d03'
+const USDC_WETH_PAIR = '0x68bf247e1f763cb157b2b5f1b927dff4522723d5'
+const DAI_WETH_PAIR = '0xd40bfd9bffc798b5183cbe27d877562fb09c8958'
+const USDT_WETH_PAIR = '0xe82b7054471d3f5cc825c50350da3bca64f7be4e'
 
 export function getEthPriceInUSD(): BigDecimal {
   // fetch eth prices for each stablecoin
@@ -40,26 +40,16 @@ export function getEthPriceInUSD(): BigDecimal {
 
 // token where amounts should contribute to tracked volume and liquidity
 let WHITELIST: string[] = [
-  '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // WETH
-  '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI
-  '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
-  '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
-  '0x0000000000085d4780b73119b644ae5ecd22b376', // TUSD
-  '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643', // cDAI
-  '0x39aa39c021dfbae8fac545936693ac917d5e7563', // cUSDC
-  '0x86fadb80d8d2cff3c3680819e4da99c10232ba0f', // EBASE
-  '0x57ab1ec28d129707052df4df418d58a2d46d5f51', // sUSD
-  '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2', // MKR
-  '0xc00e94cb662c3520282e6f5717214004a7f26888', // COMP
-  '0x514910771af9ca656af840dff83e8264ecf986ca', //LINK
-  '0x960b236a07cf122663c4303350609a66a7b288c0', //ANT
-  '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f', //SNX
-  '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e', //YFI
-  '0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8', // yCurv
-  '0x853d955acef822db058eb8505911ed77f175b99e', // FRAX
-  '0xa47c8bf37f92abed4a126bda807a7b7498661acd', // WUST
-  '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984', // UNI
-  '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599' // WBTC
+  '0xa00744882684c3e4747faefd68d283ea44099d03', // WIOTX
+  '0x62a9d987cbf4c45a550deed5b57b200d7a319632', // maticDAI
+  '0x3b2bf2b523f54c4e454f08aa286d03115aff326c', // ioUSDC
+  '0x6fbcdc1169b5130c59e72e51ed68a84841c98cd1', // ioUSDT
+  '0x3cdb7c48e70b854ed2fa392e21687501d84b3afc', // maticUSDT
+  '0x84abcb2832be606341a50128aeb1db43aa017449', // BUSD
+  '0xacee9b11cd4b3f57e58880277ac72c8c41abe4e4', // ioBUSD
+  '0x0258866edaf84d6081df17660357ab20a07d0c80', // ioETH
+  '0xc7b93720f73b037394ce00f954f849ed484a3dea', // WBTC
+  '0x8e66c0d6b70c0b23d39f4b21a1eac52bba8ed89a'  // WMATIC
 ]
 
 // minimum liquidity required to count towards tracked volume for pairs with small # of Lps
@@ -73,7 +63,7 @@ let MINIMUM_LIQUIDITY_THRESHOLD_ETH = BigDecimal.fromString('2')
  * @todo update to be derived ETH (add stablecoin estimates)
  **/
 export function findEthPerToken(token: Token): BigDecimal {
-  if (token.id == WETH_ADDRESS) {
+  if (token.id == WIOTX_ADDRESS) {
     return ONE_BD
   }
   // loop through whitelist and check if paired with any
